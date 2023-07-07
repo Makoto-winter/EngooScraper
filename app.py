2598ae0de17807f30040692c26e521cec760e73c
Makoto-winter <winter12months@gmail.com>
2023-07-07 08:15:05 -0400

    Version 2.0 Interface change

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ import brain
 import connectToMongo
 from pymongo import MongoClient
 from dotenv import load_dotenv
+import time
+
 load_dotenv()
 import bcrypt
 import os
-from flask import Flask, render_template, request, url_for, redirect, session
-
+from flask import Flask, render_template, request, url_for, redirect, session, flash
 
 # ############   connectToMongo object    ############
 test_client = MongoClient(os.getenv('ATLAS_URI'))
@@ -25,9 +26,14 @@ app.secret_key = "testing"
 
 @app.route("/", methods=['post', 'get'])
 def index():
+    return render_template('top.html')
+
+
+@app.route("/register", methods=['post', 'get'])
+def register():
     message = ''
     if "email" in session:
-        return redirect(url_for("logged_in"))
+        return redirect(url_for('logged_in'))
     if request.method == "POST":
         email = request.form.get("email")
 
@@ -37,18 +43,18 @@ def index():
         email_found = records.find_one({"email": email})
 
         if email_found:
-            message = 'This email already exists in database'
-            return render_template('index.html', message=message)
+            message = 'This email already exists in our database.'
+            return render_template('pages-register.html', message=message)
         if password1 != password2:
             message = 'Passwords should match!'
-            return render_template('index.html', message=message)
+            return render_template('pages-register.html', message=message)
         else:
             hashed = bcrypt.hashpw(password2.encode('utf-8'), bcrypt.gensalt())
             user_input = {'email': email, 'password': hashed}
             records.insert_one(user_input)
-            return redirect(url_for("login"))
+            return render_template('pages-redirect-login.html')
 
-    return render_template('index.html')
+    return render_template('pages-register.html')
 
 
 @app.route('/logged_in', methods=["POST", "GET"])
@@ -66,14 +72,13 @@ def logged_in():
             if request.form.getlist("delete") == [""]:
                 data.Remove(int(request.form.get("tutorID")), user_email)
 
-        return render_template('logged_in.html', email=user_email, tutors=tutors_col)
+        return render_template('tables-data.html', email=user_email, tutors=tutors_col)
     else:
         return redirect(url_for("login"))
 
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
-    message = 'Please login to your account'
     if "email" in session:
         return redirect(url_for("logged_in"))
 
@@ -92,24 +97,28 @@ def login():
             else:
                 if "email" in session:
                     return redirect(url_for("logged_in"))
-                message = 'Wrong password'
-                return render_template('login.html', message=message)
+                message = 'Email found. Wrong password'
+                return render_template('pages-login.html', message=message)
         else:
-            message = 'Email not found'
-            return render_template('login.html', message=message)
-    return render_template('login.html', message=message)
+            message = 'The input credentials do not exist.'
+            return render_template('pages-login.html', message=message)
+    return render_template('pages-login.html')
 
 
 @app.route("/logout", methods=["POST", "GET"])
 def logout():
     if "email" in session:
         session.pop("email", None)
-        return render_template("signout.html")
+        return render_template('pages-redirect-signout-home.html')
     else:
         return render_template('index.html')
 
+@app.route("/terms", methods=["POST", "GET"])
+def terms():
+    return render_template('terms.html')
 
 if __name__ == "__main__":
     # app.run(debug=True, use_reloader=False)
     from waitress import serve
+
     serve(app, host="0.0.0.0", port=8080)
