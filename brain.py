05f0aae6aa0a547b1d7c936a69e34be8b87f281e
Makoto-winter <winter12months@gmail.com>
2023-07-14 00:13:50 -0400

    Version 2.0.14 app.py simply run and interval min 2

--- brain.py
+++ brain.py
@@ -5,7 +5,7 @@ load_dotenv()
 import connectToMongo
 from apscheduler.schedulers.background import BackgroundScheduler
 
-intervalMinute = 10
+intervalMinute = 2
 
 test_client = MongoClient(os.getenv('ATLAS_URI'))
 
